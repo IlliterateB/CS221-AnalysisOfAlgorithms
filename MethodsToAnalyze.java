2022cabb9744ea4e7b3c3f7990d37defc99a869c
Bryson Leatham <brysonleatham@u.boisestate.edu>
2026-02-13 16:43:11 -0700

    Finished, sortIt() calcs were just done on the HW sheet.

--- MethodsToAnalyze.java
+++ MethodsToAnalyze.java
@@ -27,11 +27,11 @@ public class MethodsToAnalyze {
 	 * @param oldValue value to replace
 	 * @param newValue new value
 	 */
-	public static void replaceAll(int[] array, int oldValue, int newValue) { // WORST: (3 + 1.5n) + 2 + 1n + (3n + 1.5n^2) + 1n == 5 + 6.5n + 1.5n^2 // BEST: 4 + 3n // AVERAGE: 
-		int index = find(array, oldValue); // 1 @ call for this then 3 + 1.5n @ find
+	public static void replaceAll(int[] array, int oldValue, int newValue) { // WORST: (3 + 3(n-1)) + 2 + 1n + (2n + 1.5n^2) + 1n == 2 + 7n + 1.5n^2 // BEST: 4 + 3n // AVERAGE: 
+		int index = find(array, oldValue); // 1 @ call for this then 3 + 3(n-1) @ find with highest index
 		while (index > -1) { // Best for this is 0, but the find uses 2 + 3n // 1 @ initial check
 			array[index] = newValue; // 1n
-			index = find(array, oldValue); // n(3 + 1.5n)
+			index = find(array, oldValue); // n(2 + 1.5n)
 		} // 1n @ check loop condition
 	}
 	
@@ -39,15 +39,15 @@ public class MethodsToAnalyze {
 	 * Take an int[] and reorganize it so they are in ascending order.
 	 * @param array ints that need to be ordered 
 	 */
-	public static void sortIt(int[] array) {
-		for (int next = 1; next < array.length; next++) {
-			int value = array[next];
-			int index = next;
-			while (index > 0 && value < array[index - 1]) {
-				array[index] = array[index - 1];
-				index--;
+	public static void sortIt(int[] array) { // WORST: I just did all the math on the sheet// BEST: // AVERAGE: 
+		for (int next = 1; next < array.length; next++) {// 1 @ next=1, 1 @ next < array.length, 1n @ next++
+			int value = array[next]; // 1n
+			int index = next; // 1n
+			while (index > 0 && value < array[index - 1]) { // 1n @ index > 0, 1n @ value < array[index - 1], n(2n) @ check loop condition
+				array[index] = array[index - 1]; // n(1n)
+				index--; // n(1n)
 			}
-			array[index] = value;
+			array[index] = value; // 1n
 		}
 	}
 }
